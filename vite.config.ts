49fc637e4cc7da21d6c86e54521eefa39d754af3
spuddermax <spuddermax@gmail.com>
2025-02-17 10:12:58 -0500

    See if we can't fix the browser lockup issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    hmr: true,
-  },
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
+	server: {
+		hmr: true,
+		https: {
+			key: "/etc/ssl/certs/freecarver-key.pem",
+			cert: "/etc/ssl/certs/freecarver-cert.crt",
+		},
+	},
+	build: {
+		sourcemap: false,
+	},
 });
