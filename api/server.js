ec5b2f6ea0570478544f2c64dee71bf882c057e1
spuddermax <spuddermax@gmail.com>
2025-02-17 10:13:44 -0500

    Implement secure server

--- api/server.js
+++ api/server.js
@@ -3,11 +3,31 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import { logger } from "./logger.js";
+import fs from "fs";
+import https from "https";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	logger.info(`Server is running on port ${PORT}`);
-});
+// Use HTTPS when USE_HTTPS is set to "true" in your .env file.
+// You can define API_SSL_KEY and API_SSL_CERT for the API's certificate and key paths.
+if (process.env.USE_HTTPS === "true") {
+	const keyPath =
+		process.env.API_SSL_KEY || "/etc/ssl/certs/freecarver-api-key.pem";
+	const certPath =
+		process.env.API_SSL_CERT || "/etc/ssl/certs/freecarver-api-cert.crt";
+
+	const httpsOptions = {
+		key: fs.readFileSync(keyPath),
+		cert: fs.readFileSync(certPath),
+	};
+
+	https.createServer(httpsOptions, app).listen(PORT, () => {
+		logger.info(`HTTPS Server is running on port ${PORT}`);
+	});
+} else {
+	app.listen(PORT, () => {
+		logger.info(`HTTP Server is running on port ${PORT}`);
+	});
+}
