0d452a250406ce8978c7e0747dbe1dd86375201d
Spuddermax <spuddermax@gmail.com>
2025-04-24 23:13:30 -0600

    Update footer copyright and adjust header text in Layout component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "react";
 export function Footer() {
 	return (
 		<footer className="bottom-0 left-0 right-0 z-50 bg-white/90 dark:bg-gray-800/90 shadow-lg backdrop-blur-sm p-4 text-center text-gray-700 dark:text-gray-200">
-			&copy; {new Date().getFullYear()} Free Carver, LLC. All rights
+			&copy; {new Date().getFullYear()} Matthew Daines. All rights
 			reserved.
 		</footer>
 	);
